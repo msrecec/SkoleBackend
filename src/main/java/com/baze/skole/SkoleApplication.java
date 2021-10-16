b9b610bedfed1449fdd78feb4879cd86afe8f29d
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-16 11:07:40 +0200

    Starting Setup

---
+++ src/main/java/com/baze/skole/SkoleApplication.java
@@ -0,0 +1,13 @@
+package com.baze.skole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baze/skole/SkoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baze.skole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
