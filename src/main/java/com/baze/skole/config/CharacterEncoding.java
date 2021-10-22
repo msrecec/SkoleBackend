5b6c59b06525cd60e66ab8ca9aa3468191e8ee4a
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-22 11:47:18 +0200

    added character encoding

---
+++ src/main/java/com/baze/skole/config/CharacterEncoding.java
@@ -0,0 +1,21 @@
+package com.baze.skole.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+
+@NoArgsConstructor
+public class CharacterEncoding {
+    @Bean
+    CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
+    }
+}
+
